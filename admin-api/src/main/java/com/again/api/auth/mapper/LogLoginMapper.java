4a4b0c336d6a33e1efd5c189b17fcbd44f6bddd9
Mr-luo-hm <l2509991676@163.com>
2021-05-17 20:34:42 +0800

    add grpc lib

--- admin-api/src/main/java/com/again/api/auth/mapper/LogLoginMapper.java
+++ admin-api/src/main/java/com/again/api/auth/mapper/LogLoginMapper.java
@@ -2,7 +2,6 @@ package com.again.api.auth.mapper;
 
 import com.again.api.auth.model.entity.LogLogin;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 
 public interface LogLoginMapper extends BaseMapper<LogLogin> {
 
